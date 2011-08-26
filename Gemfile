ac45bc49c2fa4f2a8040a1c1156b3bc778568bc8
jin <jin@hukelou.com>
2011-08-26 16:37:24 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+
+platforms :ruby do
+  gem 'mysql2'
+end
+
+platforms :jruby do
+  gem 'activerecord-jdbc-adapter'
+  gem 'activerecord-jdbcmysql-adapter'
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTemplate::Application.config.secret_token = 'ab17c03a3aab9f3c5b554739e7f151a3a304efe2208a71e8aa2d5ed764c13deeb884fc977a706d6daf00470acf83f66fbbb61047d3fce3e7644fe00d2033adca'
